ab7bf150efff3b7be3dcef4269a136394d4dba0c
Charles Gao <1030587461@qq.com>
2022-02-09 19:10:52 +0800

    bugs on browser fixed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 from flask_cors import CORS
 app = Flask(__name__)
 
-CORS(app, resources=r'/*')  # 解决可能存在的跨域问题
+# CORS(app, resources=r'/*')  # 解决可能存在的跨域问题
 
 UPLOAD_FOLDER = 'upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER  # 设置文件上传的目标文件夹
@@ -47,7 +47,7 @@ def api_upload():
     file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])  # 拼接成合法文件夹地址
     if not os.path.exists(file_dir):
         os.makedirs(file_dir)  # 文件夹不存在就创建
-    f = request.files['file']  # 从表单的file字段获取文件，myfile为该表单的name值
+    f = request.files.get('file')  # 从表单的file字段获取文件，myfile为该表单的name值
     if f and allowed_file(f.filename):  # 判断是否是允许上传的文件类型
         fname = f.filename
         ext = fname.rsplit('.', 1)[1]  # 获取文件后缀
@@ -58,7 +58,7 @@ def api_upload():
         mov_photo(save_filename)  # 复制照片到smplify-x相应目录下
         return jsonify({"code": 200, "msg": "上传成功"})
     else:
-        return jsonify({"code": 400, "msg": "上传失败，文件过大或文件格式不正确"})
+        return jsonify({"code": 400, "msg": "文件格式不正确"})
 
 
 # 下载文件（暂时用不到）
